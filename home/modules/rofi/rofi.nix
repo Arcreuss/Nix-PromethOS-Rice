58492d915ba54f66a5de6d8e682015631f6bbb40
Arcreuss <63006702+Arcreuss@users.noreply.github.com>
2026-03-16 20:08:05 -0400

    update rofi-theme to display element-icon

--- home/modules/rofi/rofi.nix
+++ home/modules/rofi/rofi.nix
@@ -5,5 +5,11 @@
     enable = true;
     package = pkgs.rofi;
     theme = ./rofi-theme.rasi;
+
+    extraConfig = {
+      show-icons = true;
+      display-drun = "Apps";
+      drun-display-format = "{name}";
+    };
   };
 }
